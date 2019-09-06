7a72764718afe313c06fb785d18d8737ee51f76a
Adam Kelso <kelso.adam@gmail.com>
2019-09-06 08:40:59 -0500

    Fix casing issue when loading from composer

--- src/macros.php
+++ src/macros.php
@@ -1,9 +1,11 @@
 <?php
 
+use BlastCloud\Hybrid\Expectation;
+
 foreach (BlastCloud\Hybrid\Filters\WithEndpoint::VERBS as $verb) {
     BlastCloud\Chassis\Expectation::macro(
         strtolower($verb), 
-        function ($expectation, $url) use ($verb) {
+        function (Expectation $expectation, $url) use ($verb) {
             return $expectation->withEndpoint($url, $verb);
         }
     );
